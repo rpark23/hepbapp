cc62234dd34a3b81871761d519d9e2671da21781
rpark23 <ryanparkusa@yahoo.com>
2020-06-22 13:16:51 -1000

    quiz game title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
         <div>
-          <h1>Hep B Jeopardy!</h1>
+          <h1>Hep B Quiz Game!</h1>
           <CategoryList />
         </div>
     )
